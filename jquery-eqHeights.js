f81d63063b32d0193f3e1dc94bdd72e9e89f3c94
Marc Löhe <marcloehe@googlemail.com>
2012-03-21 13:20:55 +0100

    eqHeights: Fixed bug with empty selector

--- jquery-eqHeights.js
+++ jquery-eqHeights.js
@@ -13,18 +13,18 @@
 
 (function ($) {
 
-  $.fn.eqHeights = function() {
+  $.fn.eqHeights = function () {
 
     var el = $(this);
-    if (!el.data('eqHeights')) {
-      $(window).bind('resize.eqHeights', function() {
+    if (el.length > 0 && !el.data('eqHeights')) {
+      $(window).bind('resize.eqHeights', function () {
         el.eqHeights();
       });
       el.data('eqHeights', true);
     }
-    return el.each(function() {
+    return el.each(function () {
       var curHighest = 0;
-      $(this).children().each(function() {
+      $(this).children().each(function () {
         var el = $(this),
           elHeight = el.height('auto').height();
         if (elHeight > curHighest) {
